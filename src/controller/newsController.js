865f89384af51d5ebd90bf76899d7a65aa122f46
chuanTT <95389918+chuanTT@users.noreply.github.com>
2022-11-21 21:02:24 +0700

    Update newsController.js

--- src/controller/newsController.js
+++ src/controller/newsController.js
@@ -23,14 +23,13 @@ const DetailsNews = async (req, res) => {
   let BaseUpload = `${baseUrl}/${uploadFolder}`;
 
   let { id } = req.params;
-  
-  console.log(id);
 
-//   id = parseInt(id);
 
-//   const data = await newsModel.getDetailsNews(BaseUpload, id);
+  id = parseInt(id);
 
-//   return res.status(200).json(data);
+  const data = await newsModel.getDetailsNews(BaseUpload, id);
+
+  return res.status(200).json(data);
 };
 
 const SearchNews = async (req, res) => {
